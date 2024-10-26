3968a4e33780a2180f589c0937ac7a08757effdd
VMT1312 <vinhthiem.ma@curtin.edu.au>
2024-10-26 14:22:55 +0800

    Rate limit on shooting

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from bullet import Shot
 
 
 def main():
@@ -14,10 +15,13 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    bullets = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     
     Asteroid.containers = (asteroids, updatable, drawable)
+
+    Shot.containers = (bullets, updatable, drawable)
 
     AsteroidField.containers = (updatable)
 
--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
 from circleshape import *
+from bullet import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -36,7 +38,19 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        
+        if self.timer > 0:
+            self.timer -= dt
+        else:
+            if keys[pygame.K_SPACE]:
+                self.shoot()
+                self.timer = PLAYER_SHOT_COOLDOWN
+                
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
